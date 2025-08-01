996933719acc4a6c2a97efb161fbee3c7fae149e
yashmitha-rai <yashmitharai61@gmail.com>
2025-08-01 18:57:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-console.log("hello world")
-const add = () => {
-    const taskInput = document.getElementById("taskInput");
-    const taskValue=taskInput.value;
-    console.log(taskValue);
+console.log("Helloo")
+const add=()=>{
+    const taskInput=document.getElementById("taskInput")
+    const task=taskInput.value
+    taskInput.value=""
+    if(task!="") display(task)
+    console.log(task)
 }
-const display = (task) => {
-    const taskCount = document.getElementById("taskContainer");
 
-    console.log(taskCount)
+var counter = 1
+
+const display=(task)=>{
+    const taskCont=document.getElementById("taskContainer")
+   
+    let taskId=`task${counter}`
+    console.log(taskId)
+
+    //creating elements
+    let taskBody=document.createElement("div")
+    let taskName = document.createElement('p')
+    let deleteIcon = document.createElement('i')
+    taskBody.id=taskId
+
+    //assigning tasks
+    taskName.textContent= task
+
+
+    //Adding class names
+    taskBody.classList="w-full h-14 flex justify-between items-center px-3"
+
+    deleteIcon.classList="bg-red ri-delete-bin-5-line"
+
+
+    //Adding event Listners
+    deleteIcon.addEventListener("click", ()=>{
+        deleteTask(taskId)
+    })
+
+
+    taskName.addEventListener("click",()=> {
+        taskDone(taskId)
+    })
+
+    taskBody.appendChild(taskName)
+    taskBody.appendChild(deleteIcon)
+    taskCont.appendChild(taskBody)
+    counter++
 }
-display("hii");
+const deleteTask=(taskId)=>{
+    document.getElementById(taskId).remove()
+}
+
+const taskDone =(taskId)=>{
+    const task  = document.getElementById(taskId)
+    const task_ptag = task.querySelector('p')
+    task_ptag.classList.toggle("text-green-500")
+    task_ptag.classList.toggle("line-through")
+}
